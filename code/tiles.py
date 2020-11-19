8b116a0178ac59811d1e1f4c23e13683c9487eb1
Leli <lelilia@posteo.de>
2020-11-19 12:42:59 +0100

    running mini simulation

---
+++ code/tiles.py
@@ -0,0 +1,157 @@
+import numpy as np
+import cv2
+import time
+
+TILE_SIZE = 32
+OFS = 50
+
+MARKET = """
+##################
+##..............##
+##..b#..##..#O..##
+##..c#..##..#O..##
+##..##..##..#O..##
+##..##..##..#u..##
+##..##..##..#u..##
+##...............#
+##..C#..C#..C#...#
+##..##..##..##...#
+##...............#
+##############GG##
+""".strip()
+
+
+class SupermarketMap:
+    """Visualizes the supermarket background"""
+
+    def __init__(self, layout, tiles):
+        """
+        layout : a string with each character representing a tile
+        tile   : a numpy array containing the tile image
+        """
+        self.tiles = tiles
+        self.contents = [list(row) for row in layout.split("\n")]
+        self.xsize = len(self.contents[0])
+        self.ysize = len(self.contents)
+        self.image = np.zeros(
+            (self.ysize * TILE_SIZE, self.xsize * TILE_SIZE, 3), dtype=np.uint8
+        )
+        self.prepare_map()
+
+    def get_tile(self, char):
+        """returns the array for a given tile character"""
+        # empty shelf
+        if char == '#':
+            return self.tiles[0 : TILE_SIZE, 0 : TILE_SIZE]
+        # gate
+        elif char == 'G':
+            return self.tiles[7 * TILE_SIZE : 8 * TILE_SIZE, 3 * TILE_SIZE : 4 * TILE_SIZE]
+        # cash register
+        elif char == 'C':
+            return self.tiles[1 * TILE_SIZE : 2 * TILE_SIZE, 8 * TILE_SIZE : 9 * TILE_SIZE]
+        # eggplant
+        elif char == 'O':
+            return self.tiles[1 * TILE_SIZE: 2 * TILE_SIZE, 11 * TILE_SIZE : 12 * TILE_SIZE]
+        # pink umbrella
+        elif char == 'u':
+            return self.tiles[2 * TILE_SIZE: 3 * TILE_SIZE, 9 * TILE_SIZE : 10 * TILE_SIZE]
+        # candy 1
+        elif char == 'b':
+            return self.tiles[0 * TILE_SIZE: 1 * TILE_SIZE, 3 * TILE_SIZE : 4 * TILE_SIZE]
+        # candy 2
+        elif char == 'c':
+            return self.tiles[1 * TILE_SIZE: 2 * TILE_SIZE, 3 * TILE_SIZE : 4 * TILE_SIZE]
+        # customer ghost
+        elif char == 'g':
+            return self.tiles[7 * TILE_SIZE : 8 * TILE_SIZE, : TILE_SIZE]
+        else:
+            return self.tiles[TILE_SIZE : 2 * TILE_SIZE, 2 * TILE_SIZE : 3 * TILE_SIZE]
+
+    def prepare_map(self):
+        """prepares the entire image as a big numpy array"""
+        for y, row in enumerate(self.contents):
+            for x, tile in enumerate(row):
+                bm = self.get_tile(tile)
+                self.image[
+                    y * TILE_SIZE : (y + 1) * TILE_SIZE,
+                    x * TILE_SIZE : (x + 1) * TILE_SIZE,
+                ] = bm
+
+    def draw(self, frame, offset=OFS):
+        """
+        draws the image into a frame
+        offset pixels from the top left corner
+        """
+        frame[
+            OFS : OFS + self.image.shape[0], OFS : OFS + self.image.shape[1]
+        ] = self.image
+
+    def write_image(self, filename):
+        """writes the image into a file"""
+        cv2.imwrite(filename, self.image)
+
+class Customer:
+
+    def __init__(self, terrain_map, image, x, y):
+        self.terrain_map = terrain_map
+        self.image = image
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return(f'<Customer at {self.x}, {self.y}>')
+
+    def draw(self, frame):
+        xpos = OFS + self.x * TILE_SIZE
+        ypos = OFS + self.y * TILE_SIZE
+        frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = self.image
+
+    def move(self, direction):
+        newx = self.x
+        newy = self.y
+        if direction == 'up':
+            newy -= 1
+        elif direction == 'down':
+            newy += 1
+        elif direction == 'left':
+            newx -= 1
+        elif direction == 'right':
+            newx += 1
+
+        if self.terrain_map.contents[newy][newx] == '.':
+            self.x = newx
+            self.y = newy
+
+if __name__ == "__main__":
+
+    background = np.zeros((700, 1000, 3), np.uint8)
+    tiles = cv2.imread("./graphics/tiles.png")
+
+    market = SupermarketMap(MARKET, tiles)
+
+    # instantiate a customer
+    customer = Customer(market, market.get_tile('g'), 2, 2)
+
+    while True:
+        frame = background.copy()
+        market.draw(frame)
+        customer.draw(frame)
+
+        cv2.imshow("frame", frame)
+
+        key = chr(cv2.waitKey(1) & 0xFF)
+        if key == 'w':
+            customer.move('up')
+        elif key == 'a':
+            customer.move('left')
+        elif key == 'd':
+            customer.move('right')
+        elif key == 's':
+            customer.move('down')
+
+        if key == "q":
+            break
+
+    cv2.destroyAllWindows()
+
+    market.write_image("./graphics/supermarket.png")
